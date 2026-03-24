2fb336e1cf2aa304a2ba28876d61240ffa599385
JD Solanki <jdsolanki0001@gmail.com>
2026-03-24 22:29:16 +0530

    feat: setup nuxthub

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,27 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: ['@nuxthub/core'],
+  hub: {
+    db: {
+      dialect: 'postgresql',
+      casing: 'snake_case',
+    },
+  },
+  // $production: {
+  //   hub: {
+  //     kv: { // Recommended for auth session storage
+  //       driver: 'cloudflare-kv-binding',
+  //       namespaceId: '253e9d877c534e4ea2ebf613edb6b5d1',
+  //     },
+  //     db: {
+  //       dialect: 'postgresql',
+  //       casing: 'snake_case',
+  //       connection: {
+  //         hyperdriveId: '<id>',
+  //       },
+  //     },
+  //   },
+  // },
 })

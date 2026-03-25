96c96cccff65429c59d5181088c3bb462b2b75d5
JD Solanki <jdsolanki0001@gmail.com>
2026-03-25 12:39:23 +0530

    feat: add db hook for repro

--- server/auth.config.ts
+++ server/auth.config.ts
@@ -1,5 +1,17 @@
+import { schema } from '@nuxthub/db'
 import { defineServerAuth } from '@onmax/nuxt-better-auth/config'
 
-export default defineServerAuth({
+export default defineServerAuth(({ db }) => ({
   emailAndPassword: { enabled: true },
-})
+  databaseHooks: {
+    session: {
+      create: {
+        async after(session, _ctx) {
+          // Update last sign in time on session create on user table
+          const result = await db.select().from(schema.session).limit(1)
+          console.log("session query result:", result)
+        },
+      }
+    }
+  }
+}))

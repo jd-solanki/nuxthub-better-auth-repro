097af3f213893714d74c0acc3f0516fd581f2268
JD Solanki <jdsolanki0001@gmail.com>
2026-04-18 17:35:06 +0530

    feat: revert to standalone repro by using published versions & minimal code

--- server/auth.config.ts
+++ server/auth.config.ts
@@ -3,10 +3,6 @@ import { defineServerAuth } from '@onmax/nuxt-better-auth/config'
 export default defineServerAuth(({ db }) => ({
   emailAndPassword: {
     enabled: true,
-    password: {
-      hash: hashPassword,
-      verify: verifyPassword,
-    },
   },
   databaseHooks: {
     session: {

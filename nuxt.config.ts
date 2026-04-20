9bf99ea5e5295a01d47f3932dd5e83f43c343997
JD Solanki <jdsolanki0001@gmail.com>
2026-04-20 14:51:16 +0530

    feat: reproduce sign out issue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -43,7 +43,7 @@ export default defineNuxtConfig({
   auth: {
     redirects: {
       login: '/login',
-      guest: '/',
+      guest: '/app',
       authenticated: '/app',
       logout: '/login',
     },

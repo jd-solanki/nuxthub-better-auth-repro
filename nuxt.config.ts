ec7dbffa9e64f098f142c711e95b0331b641d104
JD Solanki <jdsolanki0001@gmail.com>
2026-04-30 19:17:06 +0530

    feat: use latest PR commit & enable dev time typecheck

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,9 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: ['@nuxthub/core', '@onmax/nuxt-better-auth'],
+  typescript: {
+    typeCheck: true,
+  },
   hub: {
     db: {
       dialect: 'postgresql',

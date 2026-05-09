59f8d6a5b1044a3515cca8906ec1127df7098897
JD Solanki <jdsolanki0001@gmail.com>
2026-05-09 18:35:23 +0530

    feat: remove old experimental code & upgrade pkg to 295 PR commit

--- server/auth.config.ts
+++ server/auth.config.ts
@@ -1,14 +1,9 @@
 import { schema } from '@nuxthub/db'
 import { defineServerAuth } from '@onmax/nuxt-better-auth/config'
-import { hashPassword, verifyPassword } from './utils/password'
 
 export default defineServerAuth(({ db }) => ({
   emailAndPassword: {
     enabled: true,
-    password: {
-      hash: hashPassword,
-      verify: verifyPassword,
-    },
   },
   databaseHooks: {
     session: {
